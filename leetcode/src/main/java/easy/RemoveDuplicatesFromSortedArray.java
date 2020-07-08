2dc68765ec202934e722be78201be83791c49890
xuxin <xuxin@qianxin.com>
2020-07-08 10:51:34 +0800

    LC#26,删除排序数组中的重复项

---
+++ leetcode/src/main/java/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,48 @@
+package easy;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ *  LeetCode;26
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+ 示例 1:
+
+ 给定数组 nums = [1,1,2],
+ 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+ 示例 2:
+ 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+ */
+public class RemoveDuplicatesFromSortedArray {
+    @Test
+    public void test(){
+        int[] arr = {1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 还是快慢指针
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        int left = 0; //慢指针
+        int right = 0; //快指针
+        while (right < nums.length){
+            //快指针向前走,>慢指针数据时 慢指针前移 赋值
+            if (nums[right] > nums[left]){
+                nums[++left] = nums[right];
+            }
+            right ++;
+        }
+        return left+1;
+    }
+}
